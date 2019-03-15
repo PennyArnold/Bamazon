3fd3e3124a9d0168808b431d60bd7fea9c2a816d
Penny <radiogaga66@gmail.com>
2019-03-15 15:22:56 -0500

    added code to query/res products - got the prompts going in node

--- server.js
+++ server.js
@@ -20,11 +20,51 @@ connection.connect(function (err) {
 var makeTable = function () {
     connection.query("SELECT * FROM products", function (err, res) {
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].itemid + 
+            console.log(res[i].itemid +
                 res[i].productname + " || " +
                 res[i].departmentname + " || " +
                 res[i].price + " || " +
                 res[i].stockquantity + "\n");
+        }
+        promptCustomer(res);
+    })
+}
+
+var promptCustomer = function (res) {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'choice',
+        message: 'What would you like to purchase? [Quit with Q]'
+    }]).then(function (answer) {
+        var correct = false;
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].productname == answer.choice) {
+                correct = true;
+                var product = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'quant',
+                    message: 'How many would you like to buy?',
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function (answer) {
+                    if ((res[id].stockquantity - answer.quant) > 0) {
+                        connection.query("UPDATE products SET stockquantity='" + (res[id].stockquantity - answer.quant) + "' WHERE productname='" + product + "'", function (err, res2) {
+                            console.log('Product Purchased!');
+                            makeTable();
+                        })
+                    } else {
+                        console.log("This is not a valid selection.");
+                        promptCustomer(res);
+                    }
+                })
+            }
         }
     })
 }
