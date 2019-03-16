d3a4fc0519356e37b87969e7d199aca6c3a32b42
Penny <radiogaga66@gmail.com>
2019-03-15 21:39:48 -0500

    got the bug corrected with the table coming back adjusted for the purchase

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
-//Required dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-//creates the connection to sql
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: 'root',
+    user: "root",
     password: 'John2012',
     database: 'Bamazon'
 })
-//lets me know if we have a connection and if not, throws an error 
+
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id ", connection.threadId);
     makeTable();
-
-    connection.end();
 })
-//collects all of the data from my table in sql and prints it to the terminal
+
 var makeTable = function () {
-    connection.query('SELECT * FROM products', function (_err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].itemid + " || " +
                 res[i].productname + " || " +
@@ -31,31 +27,29 @@ var makeTable = function () {
         promptCustomer(res);
     })
 }
-//function so that the user can select an option from the table data and purchase it from the terminal
+
 var promptCustomer = function (res) {
-    inquirer.prompt([{
+    inquirer.prompt({
         type: 'input',
         name: 'choice',
         message: 'What would you like to purchase? [Quit with Q]'
-    }]).then(function (answer) {
+    }).then(function (answer) {
         var correct = false;
-        //exits if the user types Q
-        if (answer.choice.toUpperCase() === "Q") {
+        if (answer.choice.toUpperCase() == "Q") {
+            connection.end();
             process.exit();
         }
-        //loops through response from query input
         for (var i = 0; i < res.length; i++) {
             if (res[i].productname === answer.choice) {
-                console.log(res[i].productname);
                 correct = true;
                 var product = answer.choice;
                 var id = i;
                 inquirer.prompt({
-                    type: "input",
-                    name: "quantity",
+                    type: 'input',
+                    name: 'quantity',
                     message: 'How many would you like to buy?',
                     validate: function (value) {
-                        if (isNaN(value) === false) {
+                        if (isNaN(value) == false) {
                             return true;
                         } else {
                             return false;
@@ -63,10 +57,16 @@ var promptCustomer = function (res) {
                     }
                 }).then(function (answer) {
                     if ((res[id].stockquantity - answer.quantity) > 0) {
-                        connection.query("UPDATE products SET stockquantity= " + (res[id].stockquantity - answer.quantity) + "' WHERE productname='" + product + "'", function (_err, res2) {
+                        var quantity = (res[id].stockquantity - answer.quantity);
+                        connection.query("UPDATE products SET ? WHERE ?", [{
+                            stockquantity: quantity
+                        }, {
+                            productname: product
+                        }], function (err, res2) {
+                            if (err) throw err;
                             console.log('Product Purchased!');
-                            makeTable();
                         })
+                        makeTable();
                     } else {
                         console.log("This is not a valid selection.");
                         promptCustomer(res);
@@ -74,9 +74,9 @@ var promptCustomer = function (res) {
                 })
             }
         }
-        if (i === res.length && correct === false) {
-            console.log("This is not a valid selection.");
+        if (i == res.length && correct == false) {
+            console.log('This is not a valid selection.');
             promptCustomer(res);
         }
-    })
+    });
 }
